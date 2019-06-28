23f2171be2ef6ec0afe8356a74f0099a7976991c
Rafael Acioly <aciolyr@gmail.com>
2019-06-28 00:17:10 -0300

    adds comments and validation to get a valid zip code info

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import asyncio
 import time
-from typing import Dict
-
-from middlewares import request_validation
+from http import HTTPStatus
+from typing import Dict, Union
 
 import aiohttp
 from aiohttp import web
 from loguru import logger
+
+from middlewares import request_validation
 
 endpoints = {
     "viacep":           "https://viacep.com.br/ws/{cep}/json/",
@@ -26,10 +27,14 @@ async def handler(request):
     for task_completed in asyncio.as_completed(tasks):
         cep_info = await task_completed
 
+        # if the first returned value is None
+        # continues until get a valid information
         if cep_info is None:
-            raise web.HTTPNotFound()
+            continue
 
         return web.json_response(cep_info)
+
+    raise web.HTTPNotFound()
 
 
 async def get(service_name: str, endpoint: str) -> Dict:
@@ -38,7 +43,7 @@ async def get(service_name: str, endpoint: str) -> Dict:
     async with aiohttp.ClientSession() as session:
         response = await session.get(endpoint)
 
-        if response.status == 200:
+        if response.status == HTTPStatus.OK:
             json_response = await response.json()
             json_response.update({'source': service_name})
 
@@ -50,9 +55,9 @@ async def get(service_name: str, endpoint: str) -> Dict:
             return response_parsed(json_response)
 
 
-def response_parsed(content: Dict) -> Dict:
+def response_parsed(content: Dict) -> Union[Dict, None]:
     """Normalize the response content"""
-    return {
+    payload_parsed = {
         'cidade': content.get('localidade', content.get('cidade')),
         'estado': content.get('estado', content.get('uf')),
         'bairro': content.get('bairro'),
@@ -60,8 +65,13 @@ def response_parsed(content: Dict) -> Dict:
         'source': content.get('source')
     }
 
+    # all zip codes have at minimum "state" information
+    # if this information is not present on the parsed payload
+    # then the zip code requested doesn't exist
+    return payload_parsed if payload_parsed.get('estado') else None
+
 
 if __name__ == "__main__":
-    app = web.Application(middlewares=[request_validation])
+    app = aiohttp.web.Application(middlewares=[request_validation])
     app.add_routes([web.get('/{cep}', handler)])
     web.run_app(app)
